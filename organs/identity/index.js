b55f479658a4259f72381229e0ed3a5bdf05dc02
Daniel Febrero Martin <febrero@me.com>
2020-08-16 12:40:11 +0200

    clean files

--- organs/identity/index.js
+++ organs/identity/index.js
@@ -1,5 +1,7 @@
 const { user1 } = require('../../fixtures/keypairs');
 
+const identity = require('../../../identity/fabric/fabidentity/javascript');
+
 const execute = ({method, data, socket, reason}) => {
     socket.emit('answer', JSON.stringify({
         reason,

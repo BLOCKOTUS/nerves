ab3e95da422423ee2df637c2467123b5dec5ed6a
Daniel Febrero Martin <febrero@me.com>
2020-08-16 14:53:15 +0200

    test create identity

--- organs/identity/index.js
+++ organs/identity/index.js
@@ -2,7 +2,22 @@ const { user1 } = require('../../fixtures/keypairs');
 
 const identity = require('../../../identity/fabric/fabidentity/javascript');
 
-const execute = ({method, data, socket, reason}) => {
+const post = async ({data, socket, reason}) => {
+    const args = {
+        nation: data.nation,
+        nationalId: data.nationalId,
+        encryptedData: data.encryptedData
+    }
+
+    await identity.create(args);
+
+    socket.emit('answer', JSON.stringify({
+        reason,
+        answer: {success: true}
+    }))
+}
+
+const get = ({data, socket, reason}) => {
     socket.emit('answer', JSON.stringify({
         reason,
         answer: {
@@ -10,6 +25,16 @@ const execute = ({method, data, socket, reason}) => {
             id: 1,
         }
     }))
+}
+
+const methods = {
+    post,
+    get
+}
+
+const execute = ({method, data, socket, reason}) => {
+    console.log({method, data, reason});
+    methods[method] ? methods[method]({data, socket, reason}) : null;
 }
 
 module.exports = {

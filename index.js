3dfac61bbab49c326fe291056ca3882d8e53fc74
Daniel Febrero <febrero@me.com>
2020-09-24 10:53:59 +0200

    BLOCKOTUS/organism#55 add eslint, live linter and pre-push linter

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const app = express();
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
 const { logger } = require('@tinyhttp/logger');
 
 const user = require('../organs/user/api');
 const identity = require('../organs/identity/api');
 const job = require('../organs/job/api');
+
+const app = express();
 
 app
   .use(logger())
@@ -15,9 +16,9 @@ app
   .use(bodyParser.json())
 
   .use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     next();
   })
 
@@ -27,176 +28,174 @@ app
     const username = splitted[0];
     splitted.shift();
     const rawWallet = splitted.join(':');
-    try{
+    try {
       const wallet = JSON.parse(rawWallet);
       req.method === 'GET' ? req.query.user = { username, wallet } : req.body.user = { username, wallet };
-    }catch(e) { console.log(e) }
+    } catch (e) { console.log(e); }
 
     next();
   })
 
-
   // create and register a new user
   .post('/user', (req, res) => {
-    console.log(req.body)
+    console.log(req.body);
     user
       .create(req.body)
       .then(result => res.json({
         ...result,
         success: true,
-        message: 'Successfully registered username.'
+        message: 'Successfully registered username.',
       }))
       .catch(e => {
         res.json({
           success: false,
-          message: e.message
-        })
+          message: e.message,
+        });
       });
   })
 
   // create a keypair
   .post('/user/keypair', (req, res) => {
-    console.log(req.body)
+    console.log(req.body);
     user
       .shareKeypair(req.body)
       .then(result => res.json({
         ...result,
         success: true,
-        message: 'Successfully created shared keypair.'
+        message: 'Successfully created shared keypair.',
       }))
       .catch(e => {
         res.json({
           success: false,
-          message: e.message
-        })
+          message: e.message,
+        });
       });
   })
 
   // get a keypair
   .get('/user/keypair', (req, res) => {
-    console.log(req.query)
+    console.log(req.query);
     user
       .getKeypair(req.query)
       .then(result => res.json({
         keypair: result,
         success: true,
-        message: 'Successfully got keypair.'
+        message: 'Successfully got keypair.',
       }))
       .catch(e => {
         res.json({
           success: false,
-          message: e.message
-        })
+          message: e.message,
+        });
       });
   })
 
   // create and register a new identity
   .post('/identity', (req, res) => {
-    console.log(req.body)
+    console.log(req.body);
     identity
       .create(req.body)
       .then(result => res.json({
         ...result,
         success: true,
-        message: 'Successfully created identity.'
+        message: 'Successfully created identity.',
       }))
       .catch(e => {
         res.json({
           success: false,
-          message: e.message
-        })
+          message: e.message,
+        });
       });
   })
 
   // get identity
   .get('/identity', (req, res) => {
-    console.log(req.query)
+    console.log(req.query);
     identity
       .get(req.query)
       .then(result => res.json({
         identity: result,
         success: true,
-        message: 'Successfully got identity.'
+        message: 'Successfully got identity.',
       }))
       .catch(e => {
         res.json({
           success: false,
-          message: e.message
-        })
+          message: e.message,
+        });
       });
   })
 
   // create a new job
   .post('/job', (req, res) => {
-    console.log(req.body)
+    console.log(req.body);
     job
       .create(req.body)
       .then(result => res.json({
         ...result,
         success: true,
-        message: 'Successfully created job.'
+        message: 'Successfully created job.',
       }))
       .catch(e => {
         res.json({
           success: false,
-          message: e.message
-        })
+          message: e.message,
+        });
       });
   })
 
   // list jobs
   .get('/job/list', (req, res) => {
-    console.log(req.query)
+    console.log(req.query);
     job
       .list(req.query)
       .then(result => res.json({
         list: result,
         success: true,
-        message: 'Successfully listed jobs.'
+        message: 'Successfully listed jobs.',
       }))
       .catch(e => {
         res.json({
           success: false,
-          message: e.message
-        })
+          message: e.message,
+        });
       });
   })
 
   // get job details
   .get('/job', (req, res) => {
-    console.log(req.query)
+    console.log(req.query);
     job
       .get(req.query)
       .then(result => res.json({
         job: result,
         success: true,
-        message: 'Successfully got the details of the job.'
+        message: 'Successfully got the details of the job.',
       }))
       .catch(e => {
         res.json({
           success: false,
-          message: e.message
-        })
+          message: e.message,
+        });
       });
   })
 
-   // complete a job
-   .post('/job/complete', (req, res) => {
-    console.log(req.body)
+  // complete a job
+  .post('/job/complete', (req, res) => {
+    console.log(req.body);
     job
       .complete(req.body)
       .then(() => res.json({
         success: true,
-        message: 'Successfully completed the job.'
+        message: 'Successfully completed the job.',
       }))
       .catch(e => {
         res.json({
           success: false,
-          message: e.message
-        })
+          message: e.message,
+        });
       });
   })
-
 
   .listen(3000, () => {
     console.log('listening on *:3000');
